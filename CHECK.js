aa4b4821d321f055a6bd8d273ee508a52faf3c2e
Farhad Rahimi KLIE <farhadrahimiklie786@gmail.com>
2023-10-29 04:29:18 +0430

    Add files via upload

---
+++ CHECK.js
@@ -0,0 +1,5 @@
+CHECK constraint ensures that all values in a column satisfy certain conditions
+Create table <table_name>
+( <col1> <data_type> CHECK (condition),
+<col2> <data_type>,
+<col3> <data_type>);
