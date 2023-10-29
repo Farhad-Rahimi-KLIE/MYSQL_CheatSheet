593d557f059772bbcf5fbc94ca5d757e3abf4bcb
Farhad Rahimi KLIE <farhadrahimiklie786@gmail.com>
2023-10-29 04:31:17 +0430

    Add files via upload

---
+++ Condition Based on a Range.js
@@ -0,0 +1,5 @@
+It will only retrieve data of those columns whose values will fall between value1 and value2 (both
+inclusive)
+Select <co11>, <col2> 
+From <table_name>
+Where <value1> Between <value2>;
