409810c9777677d77c368c9cdba5af3ee7f6b937
Farhad Rahimi KLIE <farhadrahimiklie786@gmail.com>
2023-10-29 04:42:25 +0430

    Add files via upload

---
+++ order by clause.js
@@ -0,0 +1,4 @@
+It will return records in the ascending order of the specified column name's data
+Select * from <table_name> order by <column_name>;
+It will return records in the descending order of the specified column name's data
+Select * from <table_name> order by <column_name> DESC;
