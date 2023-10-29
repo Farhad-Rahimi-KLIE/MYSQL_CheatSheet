b4261150cd30dc44f49c6fb74efe1dbd79bc238b
Farhad Rahimi KLIE <farhadrahimiklie786@gmail.com>
2023-10-29 04:43:52 +0430

    Add files via upload

---
+++ RIGHT OUTER JOIN.js
@@ -0,0 +1,4 @@
+It returns all rows from the RIGHT-hand table specified in the ON condition and only those rows
+from the other table where the join condition is satisfied
+
+SELECT columns FROM table1 RIGHT [OUTER] JOIN table2 ON table1.column = tabl
