a54f7030021a0f9949c02dc5b891e8471e32ff6c
Farhad Rahimi KLIE <farhadrahimiklie786@gmail.com>
2023-10-29 04:42:38 +0430

    Add files via upload

---
+++ Numeric.js
@@ -0,0 +1,8 @@
+INT -- Integer data type
+TINYINT
+SMALLINT
+MEDIUMINT
+BIGINT
+FLOAT(M,D) -- Floating point data type
+DOUBLE(M,D) -- Double data type also stores decimal values
+DECIMAL(M,D) -- Decimal data type
