b69afc7680c52df908a44cb728f0e7b7bc9b1e4f
Farhad Rahimi KLIE <farhadrahimiklie786@gmail.com>
2023-10-29 04:37:10 +0430

    Add files via upload

---
+++ INNER JOIN.js
@@ -0,0 +1,3 @@
+It returns all rows from multiple tables where the join condition is satisfied. It is the most
+common type of join.
+SELECT columns FROM table1 INNER JOIN table2 ON table1.column = table2.colum
